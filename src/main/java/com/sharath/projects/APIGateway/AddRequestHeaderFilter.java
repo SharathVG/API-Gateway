46cabb4003dd4605337fd9ac92e0b2cea6f66253
SharathVG <40023308+SharathVG@users.noreply.github.com>
2019-03-06 21:56:32 +0530

    Add files via upload

---
+++ src/main/java/com/sharath/projects/APIGateway/AddRequestHeaderFilter.java
@@ -0,0 +1,35 @@
+package com.sharath.projects.APIGateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AddRequestHeaderFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		RequestContext rtx = RequestContext.getCurrentContext();
+		rtx.addZuulRequestHeader("x-location", "Bangalore");
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
